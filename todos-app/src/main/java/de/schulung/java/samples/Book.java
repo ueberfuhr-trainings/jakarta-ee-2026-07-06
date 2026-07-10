878da732d16353e9865d100fd73f608fc4f4d55f
Ralf Ueberfuhr <ralf.ueberfuhr@atvantage.com>
2026-07-10 10:07:50 +0200

    Add exercise on Java Reflection demonstrating dynamic class manipulation

---
+++ todos-app/src/main/java/de/schulung/java/samples/Book.java
@@ -0,0 +1,20 @@
+package de.schulung.java.samples;
+
+public class Book {
+	
+	private String title;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [title=" + title + "]";
+	}
+
+}
